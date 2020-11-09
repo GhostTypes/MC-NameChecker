1270486a22135fc80106bcea35e825b57515c92b
GhostTypes <31757267+GhostTypes@users.noreply.github.com>
2020-11-09 03:28:31 -0500

    Rewrite

--- NameChecker.py
+++ NameChecker.py
@@ -1,135 +1,163 @@
-import requests, string, random, time, json, os, re, easygui
+import os, re
+from time import sleep
 from bs4 import BeautifulSoup
-from colorama import init
 from multiprocessing.dummy import Pool as ThreadPool
-from colorama import Fore, Back, Style
+from colorama import Fore, init
 from ctypes import windll
-clear = lambda: os.system('cls')
-clear()
-init(autoreset=True)
+import requests
+from requests import Session
+from random import choice
+from threading import Thread, Lock
+from easygui import fileopenbox
 
-#Init
-windll.kernel32.SetConsoleTitleW(f'NameChecker by NotGhostTypes#0872')
-green = Fore.LIGHTGREEN_EX
-red = Fore.LIGHTRED_EX
-blue = Fore.LIGHTBLUE_EX
-print(f'{green}Name Checker starting...')
-proxyList = requests.get('https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=4000&country=all&ssl=all&anonymity=all').text.splitlines()
-print(f'{green}Scraped {len(proxyList)} proxies.')
-goodAccounts = []
-badAccounts = []
-upcomingAccounts = []
-time.sleep(2)
-clear()
+class Stats:
+    cpm = 0
+    good = 0
+    bad = 0
+    upcoming = 0
+    error = 0
+    blocked = 0
+    checked = 0
 
-def getProxy():
-    proxy = random.choice(proxyList)
-    line = f"socks4://{proxy}"
-    proxy_form = {'http': line, 'https': line}
-    return proxy, proxy_form
+class Results:
+    good = []
+    upcoming = []
 
-def genNames(min, max, total):
-    nameList = []
-    dict = string.ascii_letters + string.digits
-    while len(nameList) < total:
-        n = ''.join((random.choice(dict) for i in range(random.randint(min, max))))
-        if n not in nameList:
-            nameList.append(n)
-    return nameList
-
-def saveList(list, outPath): #Save list to file
-    with open(outPath, 'w') as f:
-        f.write("\n".join(str(item) for item in list))
-    return
-
-
-choice = "invalid"
-while choice == "invalid":
-    print(f'{blue}NameChecker by NotGhostTypes#0872')
-    print(f'{blue}[1] Generate Names')
-    print(f'{blue}[2] Load Names from File')
-    print(f'{red}[3] Exit')
-    x = input()
-    if "1" in x:
-        print(f'{blue}Min Username Length?')
-        minLen = int(input())
-        print(f'{blue}Max Username Length?')
-        maxLen = int(input())
-        print(f'{blue}Total Usernames?')
-        total = int(input())
-        nameList = genNames(minLen, maxLen, total)
-        clear()
-        print(f'{green}Generated {len(nameList)} names')
-        nameListSize = len(nameList)
-        break
-    elif "2" in x:
-        nameList = open(easygui.fileopenbox(title="Load Name List", default="*.txt"), 'r').read().splitlines()
-        nameListSize = len(nameList)
-        os.system('cls')
-        print(f'{green}Loaded {len(nameList)} names from file')
-        break
-    elif "3" in x:
-        clear()
-        print(f'{red}See you next time!')
+class Main:
+    def __init__(self):
+        self.isRunning = True
+        self.proxylist = []
+        self.namelist = []
+        self.namelistsize = 0
+        self.loadProxy()
+        self.loadNames()
+        Thread(target=self.cpmCounter, daemon=True).start()
+        self.initiate()
+        self.saveHits()
         exit()
-    else:
-        choice == "invalid"
-        print(f'{Fore.LIGHTRED_EX}Invalid choice. Please try again.')
-        time.sleep(3)
 
-def checkName(name):
-    checked = len(badAccounts) + len(goodAccounts)
-    windll.kernel32.SetConsoleTitleW(f'NameChecker by NotGhostTypes#0872 | Checked: [{checked} / {nameListSize}] | Good: {len(goodAccounts)} | Bad: {len(badAccounts)} | Upcoming: {len(upcomingAccounts)}')
-    validRequest = False
-    retries = 0
-    while not validRequest:
-        proxy = getProxy()
+    def initiate(self):
+        clear()
+        mainpool = ThreadPool(processes=int(input('Threads?: ')))
+        Thread(target=self.title).start()
+        clear()
+        mainpool.imap_unordered(func=self.checkName, iterable=self.namelist)
+        mainpool.close()
+        mainpool.join()
+        self.isRunning = False
+        clear()
+        print(f'{blue}Hits: {Stats.good}\nUpcoming: {Stats.upcoming}\nBad: {Stats.bad}')
+
+    def title(self):
+        while self.isRunning:
+            windll.kernel32.SetConsoleTitleW(
+                f'NameChecker by NotGhostTypes#0872'
+                f' | [{Stats.checked} / {self.namelistsize}'
+                f' | Hits: {Stats.good}'
+                f' | Upcoming: {Stats.upcoming}'
+                f' | Error: {Stats.error}'
+                f' | Bad: {Stats.bad}'
+                f' | Blocked: {Stats.blocked}'
+                f' | CPM: {Stats.cpm}'
+                f' | Proxies: {len(self.proxylist)}'
+            )
+
+    def loadNames(self):
+        while True:
+            try:
+                print(f"{blue}Load your name list...")
+                sleep(0.3)
+                loader = open(fileopenbox(title="Load Name List", default="*.txt"), 'r', encoding="utf8", errors='ignore').read().split('\n')
+                self.namelist = list(set(x.strip() for x in loader if x != ''))
+                if len(self.namelist) == 0:
+                    print(f'{red}No names found!\n')
+                    continue
+                print(f"Imported {len(self.namelist)} lines")
+                break
+            except:
+                exit()
+        self.namelistsize = len(self.namelist)
+
+    def cpmCounter(self):
+        while self.isRunning:
+            if Stats.checked >= 1:
+                now = Stats.checked
+                sleep(3)
+                Stats.cpm = (Stats.checked - now) * 20
+
+    def prints(self, line):
+        lock.acquire()
+        print(line)
+        lock.release()
+
+    def loadProxy(self):
+        loader = requests.get('https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all').text.split('\n')
+        self.proxylist = list(set([x.strip() for x in loader if ":" in x and x != '']))
+
+    def formatProxy(self, proxy):
+        return {'http': f"socks4://{proxy}", 'https': f"socks4://{proxy}"}
+
+    def checkName(self, name):
+        validRequest = False
+        while not validRequest:
+            proxy = choice(self.proxylist)
+            broxy = self.formatProxy(proxy)
+            try:
+                r = requests.get(url=f'{nameUrl}{name}', proxies=broxy, timeout=3)
+                if r.status_code == 200:
+                        break
+            except:
+                if len(self.proxylist) >= 1000:
+                    try:
+                        self.proxylist.remove(proxy)
+                    except:
+                        None
+        b = BeautifulSoup(r.text, features='html.parser')
+        d = str(b.find(attrs={"name": re.compile(r'description', re.I)}))
         try:
-            r = requests.get(url=f'https://namemc.com/search?q={name}', proxies=proxy[1], timeout=3)
-            if r.status_code == 200:
-                validRequest = True
+            searches = str(d.split("Searches: ")[1].split("/")[0].replace(" ", ""))
         except:
-            if len(proxyList) >= 1000:
-                try:
-                    proxyList.remove(proxy[0])
-                except:
-                    None
-    del proxy
-    b = BeautifulSoup(r.text, features='html.parser')
-    d = str(b.find(attrs={"name": re.compile(r'description', re.I)}))
-    try:
-        searches = str(d.split("Searches: ")[1].split("/")[0].replace(" ", ""))
-    except:
-        searches = "Err"
-    if "Available*" in d and "Time of" not in d:
-        if int(searches) > 100:
-            print(f'{red}[BLOCKED] {name} | Searches: {searches}')
-            badAccounts.append(name)
+            searches = "Err"
+        if "Available*" in d and "Time of" not in d:
+            if int(searches) < 100:
+                Stats.good += 1
+                Results.good.append(f'{name} | Searches: {searches}')
+                self.prints(f'{green}[OPEN] {name} | {blue}Searches: {yellow}{searches}')
+            else:
+                Stats.blocked += 1
+                self.prints(f'{red}[BLOCKED] {name} | {blue}Searches: {yellow}{searches}')
+        elif "Time of" in d and "Unavailable" not in d:
+            try:
+                dt = d.split(",")[0].split('"')[1].split(' ')[3].split('T')
+            except:
+                dt = "Err"
+            self.prints(f'{yellow}[DROPPING] {name} | {blue}Drop Time: {green} {dt[0]} / {dt[1]} | {blue} Searches: {yellow}{searches}')
+            Stats.upcoming += 1
+            Results.upcoming.append(f'{name} | {dt[0]} / {dt[1]} | Searches: {searches}')
         else:
-            print(f'{green}[OPEN] {name}')
-            goodAccounts.append(f'{name} | Searches: {searches}')
-    elif "Time of" in d and "Unavailable" not in d:
-        try:
-            dt = d.split(",")[0].split('"')[1].split(' ')[3].split('T')
-        except:
-            dt = "Err"
-        print(f'{red}[DROPPING] {name} AT {dt[0]} | {dt[1]}')
-        upcomingAccounts.append(f'{name} opens at {dt}| Searches: {searches}')
-    else:
-        badAccounts.append(name)
+            Stats.bad += 1
+        Stats.checked += 1
 
-time.sleep(2)
-clear()
-print(f'{Fore.LIGHTCYAN_EX}Threads?')
-threads = int(input())
-print(f'{green}Starting threads...')
-pool = ThreadPool(threads)
-clear()
-results = pool.map(checkName, nameList)
-pool.close()
-pool.join()
-print(f'{green}Checker finished.')
-if len(goodAccounts) >= 1:
-    saveList(goodAccounts, 'open_names.txt')
-if len(upcomingAccounts) >= 1:
-    saveList(upcomingAccounts, 'upcoming_names.txt')
+    def saveHits(self):
+        if Stats.good >= 1:
+            with open('open_names.txt', 'w') as f:
+                for name in Results.good:
+                    f.write(f'{name}\n')
+        if Stats.upcoming >= 1:
+            with open('upcoming_names.txt', 'w') as f:
+                for name in Results.upcoming:
+                    f.write(f'{name}\n')
+
+
+if __name__ == '__main__':
+    init()
+    clear = lambda: os.system('cls')
+    blue = Fore.LIGHTBLUE_EX
+    red = Fore.LIGHTRED_EX
+    green = Fore.LIGHTGREEN_EX
+    yellow = Fore.LIGHTYELLOW_EX
+    lock = Lock()
+    session = Session()
+    nameUrl = 'https://namemc.com/search?q='
+    windll.kernel32.SetConsoleTitleW('NameChecker by NotGhostTypes#0872')
+    Main()
